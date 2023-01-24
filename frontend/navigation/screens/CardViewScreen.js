5cacc2a1bc7cde957113ffd5736812279e1c065d
Phu <phulam11031996@gmail.com>
2023-01-23 19:39:25 -0800

    done with navigation

---
+++ frontend/navigation/screens/CardViewScreen.js
@@ -0,0 +1,13 @@
+
+import * as React from "react";
+import { Text, View} from "react-native";
+
+const CardViewScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>CardView!</Text>
+    </View>
+  );
+}
+
+export default CardViewScreen;
